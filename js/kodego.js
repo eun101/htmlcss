7f3440fc0d089d7cd358e4159671fb523c0f3c82
tereriphiceun@gmail.com <tereriphiceun@gmail.com>
2022-10-22 02:05:27 +0800

    updates exercises 3-5

--- js/kodego.js
+++ js/kodego.js
@@ -1,4 +1,4 @@
-function OddEvenNumbers (OENumbers)
+function OddEvenNumbers (OENumbers){
     switch (OENumbers){
         case "a":
         case "a":
@@ -6,5 +6,21 @@ function OddEvenNumbers (OENumbers)
         case "a":
         case "a":
 
+        }
+    }
 
+
+    function foo() {
+        alert("Thisss");
+    }
+
+
+
+    function forr(arr) {
+        alert(arr.length);
+    }
+
+
+    function addnumbers(a,b){
+        console.log(a + b);
     }
